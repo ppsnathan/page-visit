9f18817bb041f0281cb6d7214ee9fa22f0ec5646
Shan <psnathan@gmail.com>
2020-02-23 19:27:52 +0800

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node:latest
+
+WORKDIR /app
+
+COPY package.json  .
+COPY server.js  .
+
+RUN npm install
+
+EXPOSE 3000
+
+CMD node server.js
+
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const REDIS_URL = process.env.REDIS_URL
+const redis = require("redis");
+const client = redis.createClient(REDIS_URL);
+
+app.get('/', (request, response) => {
+	console.log('request')
+	client.incr("visit", function(err, res) {
+		response.send("This is the " + res + " visitor>")
+	})
+})
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`))
